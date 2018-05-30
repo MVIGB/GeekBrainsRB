c649e98d5e4d223ceab3a4fb88c9e0cdedeaea39
MVIGB <Vvovo@mail.ru>
2018-05-30 13:16:56 +0300

    init

--- task1.js
+++ task1.js
@@ -24,32 +24,11 @@ for (var i = 0; i < upperInputString.length; i++)
 {
 switch(upperInputString[i])
 {
-case 'A':
-case 'B':
-case 'C':
-case 'D':
-case 'E':
-case 'F':
-case 'G':
-case 'H':
-case 'I':
-case 'J':
-case 'K':
-case 'L':
-case 'M':
-case 'N':
-case 'O':
-case 'P':
-case 'Q':
-case 'R':
-case 'S':
-case 'T':
-case 'U':
-case 'V':
-case 'W':
-case 'X':
-case 'Y':
-case 'Z':
+case 'A': case 'B': case 'C': case 'D': case 'E':
+case 'F': case 'G': case 'H': case 'I': case 'J':
+case 'K': case 'L': case 'M': case 'N': case 'O':
+case 'P': case 'Q': case 'R': case 'S': case 'T':
+case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
 
 normalizeUpperInputString += upperInputString.charAt(i);
 break;
