eacb8dd6f9631c3c3c86b1c7aa04ca74df9fcdc0
MVIGB <Vvovo@mail.ru>
2018-05-30 13:20:24 +0300

    Initial commit

--- task1.js
+++ task1.js
@@ -28,7 +28,8 @@ case 'A': case 'B': case 'C': case 'D': case 'E':
 case 'F': case 'G': case 'H': case 'I': case 'J':
 case 'K': case 'L': case 'M': case 'N': case 'O':
 case 'P': case 'Q': case 'R': case 'S': case 'T':
-case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
+case 'U': case 'V': case 'W': case 'X': case 'Y':
+case 'Z':
 
 normalizeUpperInputString += upperInputString.charAt(i);
 break;
