2d0fe61a9a72b34bc2b6e8f1d43910044929de64
MVIGB <Vvovo@mail.ru>
2018-06-02 09:40:25 +0300

    version 3.0

--- task3.rb
+++ task3.rb
@@ -1,5 +1,45 @@
 # Task 3
 
-if ARGV[0].length >= 1 && ARGV[0].length <= 10000
-puts ARGV[0].delete('^AEIOUaeiou') + ' ' + ARGV[0].delete('AEIOUaeiou0-9 ').delete('^a-zA-Z') + ' ' + ARGV[0].delete('^0-9')
+inputString = ARGV[0]
+
+if inputString.length >= 1 && inputString.length <= 10000
+
+vowels = inputString.delete('^AEIOUaeiou')
+consonants = inputString.delete('AEIOUaeiou0-9 ').delete('^a-zA-Z')
+digits = inputString.delete('^0-9')
+
+code = inputString.delete('^AEIOUaeiou').length == 0 ? '0' : '1'
+code += inputString.delete('AEIOUaeiou0-9 ').delete('^a-zA-Z').length == 0 ? '0' : '1'
+code += inputString.delete('^0-9').length == 0 ? '0' : '1'
+
+case code
+
+when '111'
+puts vowels + ' ' + consonants + ' ' + digits
+
+when '110'
+puts vowels + ' ' + consonants
+
+when '101'
+puts vowels + ' ' + digits
+
+when '100'
+puts vowels
+
+when '011'
+puts consonants + ' ' + digits
+
+when '010'
+puts consonants
+
+when '001'
+puts digits
+
+when '000'
+puts ''
+
+else
+pust 'Something is wrong!'
+end
+
 end
