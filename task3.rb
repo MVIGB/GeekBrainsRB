e35867c1a80405ddc62c08e760b4fdc8e6f342e9
MVIGB <Vvovo@mail.ru>
2018-06-09 11:11:30 +0300

    version 4.5

--- task3.rb
+++ task3.rb
@@ -4,9 +4,9 @@ inputString = ARGV[0]
 
 if inputString.length >= 1 && inputString.length <= 10000
 
-vowels = inputString.delete('^AEIOUaeiou')
-consonants = inputString.delete('AEIOUaeiou0-9 ').delete('^a-zA-Z')
-digits = inputString.delete('^0-9')
+vowels = inputString.gsub(/[^aeiou]/i, '')
+consonants = inputString.gsub(/[^a-z]|[aeiou]/i, '')
+digits = inputString.gsub(/\D/, '')
 
 outputString = vowels + ' ' + consonants + ' ' + digits
 
