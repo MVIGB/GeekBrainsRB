943e9cfc24b73415d40d610680852f7c1e921284
MVIGB <Vvovo@mail.ru>
2018-06-10 14:02:46 +0300

    version 4.5

--- task2.rb
+++ task2.rb
@@ -6,14 +6,14 @@ if inputNumber >= -10000 && inputNumber <= 10000
 
 fibo = 0; fibo_prev = 1
 
-for i in 1..inputNumber.abs
+inputNumber.abs.times do |i|
 
 fibo = fibo_prev + fibo
 fibo_prev = fibo - fibo_prev
 
 end
 
-fibo *= -1 if inputNumber < 0 && (inputNumber.abs % 2).zero?
+fibo *= -1 if inputNumber < 0 && (inputNumber % 2).zero?
 
 print fibo
 
